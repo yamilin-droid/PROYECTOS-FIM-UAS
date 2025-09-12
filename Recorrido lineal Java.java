ae04be95d0fffff7485238fdf0ca1ae82aa156f3
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-12 01:33:30 -0600

    Recorrido lineal Java.java

--- Recorrido lineal Java.java
+++ Recorrido lineal Java.java
@@ -1,17 +1,17 @@
 public class Array { 
     public static void main(String[] args) {
         int[] edades = {23, 25, 36, 24, 37, 43, 38};
-        //recorriendo los elementos con un ciclo for
+        
         for (int i = 0; i < edades.length; i++) {
             System.out.println("Indice: " + i + "| "+" Valor: " + edades[i]);
         }
-        // Insertar (reemplazar valor en la posición 0)
+      
         edades[0] = 29;
         System.out.println("Edades después de añadir 29 remplazando la primera edad: ");
         for (int edad : edades) {
             System.out.println(edad);
         }
-        //buscando el elemento 25 con busqueda lineal
+        
         int posi = busquedalineal(edades, 25);
         if (posi != -1) {
             System.out.println("Edad 25 encontrada en indice: " + posi);
@@ -19,7 +19,6 @@ public class Array {
             System.out.println("Edad 25 no encontrada");
         }
     }
-    //metodo para busqueda lineal
     public static int busquedalineal(int[] edades, int buscado) {
         for (int i = 0; i < edades.length; i++) {
             if (edades[i] == buscado) {
@@ -29,3 +28,4 @@ public class Array {
         return -1; 
     }
 }
+
