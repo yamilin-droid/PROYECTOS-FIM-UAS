f8dba301a1e8a43170caf25797ad00c1ff1850d9
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-12 01:07:41 -0600

    Arreglo abstracto Python.py

--- Arreglo abstracto Python.py
+++ Arreglo abstracto Python.py
@@ -1,21 +1,20 @@
-#creando un tipo de dato abstracto(persona)
 class persona:
     def __init__(self,nombre,edad):
         self.nombre = nombre
-        self.edad = edad
-    #metodo para mostrar la informacion           
+        self.edad = edad         
     def mostrar(self):
         print(f"Nombre: {self.nombre} Edad: {self.edad}")
-#creando un arreglo para el dato abstracto
+
 personas = [
-    persona("Ana", 20),
-    persona("Luis", 21),
-    persona("Clara", 33),
+    persona("Ramon", 20),
+    persona("Yamill", 21),
+    persona("Cesar", 33),
     persona("Dana", 26),
     persona("Pablo", 24),
-    persona("Diego", 37)
+    persona("Paco", 37)
 ]
-#mostrando los datos del arreglo con un ciclo for each
+
 for p in personas:
     p.mostrar()
+
     
