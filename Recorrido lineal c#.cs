8463e970d0f7ad15cc63a8d551acbe45058bb976
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-12 01:36:35 -0600

    Recorrido lineal c#.cs

--- Recorrido lineal c#.cs
+++ Recorrido lineal c#.cs
@@ -4,7 +4,7 @@ class Program
 {
     static int BusquedaLineal(int[] edades, int buscado)
     {
-        //busqueda lineal
+        
         for (int i = 0; i < edades.Length; i++)
         {
             if (edades[i] == buscado)
@@ -19,14 +19,14 @@ class Program
     {
         int[] edades = { 23, 25, 36, 24, 37, 43, 38 };
 
-        //recorriendo el arreglo
+       
         Console.WriteLine("Recorrido del arreglo:");
         for (int i = 0; i < edades.Length; i++)
         {
             Console.WriteLine("Índice: " + i + " Valor: " + edades[i]);
         }
 
-        //añadiendo un nuevo elemento remplazando otro
+        
         edades[5] = 19;
         Console.WriteLine("Edades después de añadir la edad 19:");
         foreach (int edad in edades)
@@ -34,7 +34,7 @@ class Program
             Console.WriteLine(edad);
         }
 
-        //buscando el numero 36 con una busaqueda lineal
+        
         int pos = BusquedaLineal(edades, 36);
         if (pos != -1)
         {
@@ -46,3 +46,4 @@ class Program
         }
     }
 }
+
