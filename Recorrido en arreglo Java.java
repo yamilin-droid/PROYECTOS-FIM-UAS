7cac292d06eb6ba5498d838d063a1c739e38b371
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-12 01:17:27 -0600

    Recorrido en arreglo Java.java

--- Recorrido en arreglo Java.java
+++ Recorrido en arreglo Java.java
@@ -1,15 +1,14 @@
 public class Array{
-    //creando un arreglo (matriz)
     public static void main(String[] args) {
         int[][] edades ={
             {22,36,43},
             {31,27,19},
             {24,32,47}
         };
-        //devolviendo el tamaño de las filas
+    
         int filas = edades.length;
         int columnas = edades[0].length;
-        //recorriendo el arreglo por columnas con un ciclo anidado
+        
         for (int i = 0; i < columnas; i++) {
             for (int j = 0; j < filas; j++) {
                 System.out.println(edades[i][j] + " ");
@@ -17,4 +16,5 @@ public class Array{
             System.out.println();
         }
     }
+
 }
