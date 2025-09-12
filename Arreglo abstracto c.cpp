86ef222f04c9fdbe8a7be8ca506937e57f56c1ad
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-12 01:03:03 -0600

    Arreglo abstracto c.cpp

--- Arreglo abstracto c.cpp
+++ Arreglo abstracto c.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//creando un tipo de dato abstracto(persona)
 class persona{
     string nombre;
     int edad;
@@ -9,14 +8,12 @@ class persona{
     persona(string n, int e){
         nombre = n;
         edad = e;
-    }
-    //metodo para mostrar la informacion 
+    } 
     void mostrar(){
         cout << "Nombre: " << nombre << " Edad: " << edad << endl;
     }
 };
 int Dato_abstracto(){
-    //creando un arreglo
     persona personas[6] = {
         persona("Ana", 20),
         persona("Luis", 25),
@@ -25,10 +22,11 @@ int Dato_abstracto(){
         persona("Victor", 32),
         persona("Maria", 37)
     };
-    //mostrando el arreglo con ciclo for
+
     for (int i = 0; i < 6; i++){
     personas[i].mostrar();
 
     }
     return 0;
+
 }
