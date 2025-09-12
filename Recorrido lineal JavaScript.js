19dbcf688423d8b2be85170927973d60e4c5d8de
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-12 01:34:10 -0600

    Recorrido lineal JavaScript.js

--- Recorrido lineal JavaScript.js
+++ Recorrido lineal JavaScript.js
@@ -1,17 +1,16 @@
-//creando el arreglo
 let edades = [23,25,36,24,37,43,38];
-//recorriendo el arreglo
+
 console.log("Edades: ");
 for(let i = 0; i < edades.length; i++) {
     console.log("Indice: " + i + "| Edad: " + edades[i])
 }
-//añadiendo un elemento reamplazando el anterior
+
 edades[3] = 19;
 console.log("Edades despues de remplazar la edad del indice 3: ")
 for(let edad of edades) {
     console.log(edad)
 }
-//funcion para la busqueda lineal
+
 function busqueda_lineal(edades, buscado) {
     for(let i = 0; i < edades.length; i++) {
         if(edades[i] == buscado) {
@@ -20,10 +19,11 @@ function busqueda_lineal(edades, buscado) {
     }
     return -1
 }
-//busqueda lineal del elemento 25
+
 let pos = busqueda_lineal(edades, 25)
 if (pos !== -1) {
     console.log("La edad 25 se encuentra en el indice: " + pos)
 } else {
     console.log("La edad 25 no se encuentra")
+
 }
