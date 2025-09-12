467aed8f5ba19cf136f830f6b556704587616725
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-12 01:05:15 -0600

    Arreglo abstracto Javascript.js

--- Arreglo abstracto Javascript.js
+++ Arreglo abstracto Javascript.js
@@ -1,25 +1,25 @@
-//creando un tipo de dato abstracto(persona)
+
 class persona {
   constructor(nombre, edad) {
     this.nombre = nombre;
     this.edad = edad;
   }
-  //metodo para mostrar la informacion
+
   mostrar() {
     console.log(`Nombre: ${this.nombre}, Edad: ${this.edad}`);
   }
 }
-//declarando un arreglo 
+
 let personas = [
-    new persona("Ana", 20),
-    new persona("Luis", 25),
-    new persona("Clara", 30),
-    new persona("Pablo", 24),
-    new persona("Victor", 32),
-    new persona("Maria", 37)
+    new persona("Ramon", 20),
+    new persona("Yamill", 25),
+    new persona("Cesar", 30),
+    new persona("Patricio", 24),
+    new persona("Miguel", 32),
+    new persona("Paco", 37)
 ] 
-//mostrando los elementos del arreglo for of
 console.log("Alumnos: ")
 for(let p of personas){
     p.mostrar();
+
 }
