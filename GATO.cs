b0e071aac3c287fc66e1dcfaa70f65b407471530
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-12 11:34:59 -0600

    GATO.cs
    Profe pongame 10

--- GATO.cs
+++ GATO.cs
@@ -56,9 +56,9 @@ class TicTacToe
     static bool CheckWin()
     {
         int[,] winPositions = {
-            {0,1,2}, {3,4,5}, {6,7,8}, // filas
-            {0,3,6}, {1,4,7}, {2,5,8}, // columnas
-            {0,4,8}, {2,4,6}           // diagonales
+            {0,1,2}, {3,4,5}, {6,7,8}, 
+            {0,3,6}, {1,4,7}, {2,5,8}, 
+            {0,4,8}, {2,4,6}           
         };
 
         for (int i = 0; i < winPositions.GetLength(0); i++)
