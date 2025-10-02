57001da77a4a66b49f60cdbb8a760791aa20d656
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-10-02 09:34:37 -0700

    Arreglo Python documentado.py

--- Arreglo Python.py
+++ Arreglo Python.py
@@ -1,2 +1,6 @@
+# Se asignan los valores 1, 2 y 3 respectivamente a las variables 'a', 'b' y 'c' usando asignación múltiple
 a, b, c = 1, 2, 3
+
+# Se crea una lista llamada 'arr' que contiene los valores de las variables 'a', 'b' y 'c'
+# El resultado será: [1, 2, 3]
 arr = [a, b, c]
