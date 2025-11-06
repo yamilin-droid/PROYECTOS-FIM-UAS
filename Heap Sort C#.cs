996bc1044da70a1b47a8f79d13928dafa66d8348
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-11-06 12:38:28 -0700

    Add files via upload

---
+++ Heap Sort C#.cs
@@ -0,0 +1,47 @@
+using System;
+
+class HeapSort {
+    static void heapSort(int[] arr) {
+        int n = arr.Length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    static void Main() {
+        int[] arr = {2, 6, 1, 4, 7, 3, 9, 10};
+        heapSort(arr);
+        Console.Write("Sorted array: ");
+        foreach (int num in arr) {
+            Console.Write(num + " ");
+        }
+    }
+}
---
+++ HeapSort Python.py
@@ -0,0 +1,29 @@
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+# Test
+arr = [2, 6, 1, 4, 7, 3, 9, 10]
+heap_sort(arr)
+print("Sorted array:", arr)
